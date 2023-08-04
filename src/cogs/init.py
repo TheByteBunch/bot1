a7b4c2699b49dc5ccba26f2bd7b154dd005c298f
Al1babax <sami.viik2@gmail.com>
2023-08-04 19:43:06 +0300

    small fix

--- src/cogs/init.py
+++ src/cogs/init.py
@@ -4,18 +4,19 @@ from discord import app_commands
 
 
 class Init(commands.Cog):
+    # Hardcoded values
+    guild = 1136642484697583667
+    user_id = 207813174588604418
 
-    def __init__(self, client:commands.Bot):
+    def __init__(self, client: commands.Bot):
         self.client = client
-        self.guild = 1136642484697583667
-        self.user_id = 207813174588604418
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("Init cog ready")
 
     @commands.command()
-    async def sync(self, ctx:commands.Context):
+    async def sync(self, ctx: commands.Context):
         print("Syncing commands")
         if ctx.author.id == self.user_id:
             synced_commands = await self.client.tree.sync(guild=discord.Object(id=self.guild))
