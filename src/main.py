18ed1d696d7a9970fe6bd933d5c8b8135402ac8d
Al1babax <sami.viik2@gmail.com>
2023-08-04 21:24:00 +0300

    feature flags

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 import discord
 from discord.ext import commands
+from configparser import ConfigParser
 
 # Importing utils
 import utils.basic as basic
@@ -22,16 +23,44 @@ async def on_ready():
     print("Bot is ready")
 
 
+def parse_feature_flags():
+    """
+    Parse feature flags from the config.ini file
+    :return:
+    """
+    # Create config parser
+    config = ConfigParser()
+    # Read the config file
+    config.read("../config.ini")
+    # Get the feature flags
+    feature_flags = config["FEATURE_FLAGS"]
+
+    # Return the feature flags
+    return feature_flags
+
+
 async def load() -> None:
     """
     Loads all the cogs in the cogs folder
     :return: None
     """
+    # Getting feature flags
+    feature_flags = parse_feature_flags()
+
     print("#" * 50)
     print(f"[INFO] Loading cogs")
+    # Looping through all the files in the cogs folder
     for filename in os.listdir("cogs"):
+        # Check if the file is a python file
         if filename.endswith(".py"):
-            await client.load_extension(f"cogs.{filename[:-3]}")
+            # Get the name of the file
+            feature_file_name = filename[:-3]
+            # Check if the feature flag is set to 0 if so skip the file
+            if feature_file_name in feature_flags and feature_flags[feature_file_name] == "0":
+                print(f"Skipping {filename}")
+                continue
+            # Load the cog
+            await client.load_extension(f"cogs.{feature_file_name}")
             print(f"Loaded {filename}")
     print("#" * 50)
 
